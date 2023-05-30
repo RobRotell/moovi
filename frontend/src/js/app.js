d71d73bc9f375f1cff24181ffffbf1256d19d429
Rob <dev@robrotell.com>
2023-05-29 21:38:39 -0400

    Building frontend

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,90 +1,10 @@
 import Alpine from 'alpinejs'
+import Moovi from './moovi'
 
 
-( () => {
+// assigning to window to help browser extensions detect AlpineJS on page
+window.Alpine = Alpine
 
 
-	const moovi = {
-
-		states: {
-			isLoading: false,
-			isError: false,
-		},
-
-
-		data: {
-			movie: {
-				title: '',
-				tagline: '',
-				director: '',
-				year: '',
-				image: '',
-			},
-			error: '',
-		},
-
-
-		/**
-		 * Initiate the good stuff
-		 *
-		 * @return {void}
-		 */
-		init() {
-			const movie = this.fetchMovie()
-		},
-
-
-		/**
-		 * Fetch movies from backend
-		 *
-		 * @return {Promise}
-		 */
-		fetchMovie() {
-			return new Promise( ( resolve, reject ) => {
-				fetch( 'https://moovi.robr.app/endpoint/get-movie' )
-					.then( res => {
-						console.log( res )
-						return res.json()
-					})
-					.then( res => {
-						console.log( res )
-					})
-			})
-		},
-
-
-		/**
-		 * Show error message
-		 *
-		 * @param {string} msg Error message
-		 * @return {self}
-		 */
-		showError( msg = '' ) {
-			return this
-		},
-
-
-		/**
-		 * Display movie on frontend
-		 *
-		 * @param {obj} movie
-		 * @return {self}
-		 */
-		displayMovie( movie ) {
-			this.data.movie = movie
-
-			return this
-		},
-
-	}
-
-
-
-	document.addEventListener( 'alpine:init', () => {
-		Alpine.store( 'moovi', moovi )
-	})
-
-	Alpine.start()
-
-
-})()
+Alpine.data( 'moovi', Moovi )
+Alpine.start()
---
+++ frontend/src/js/moovi.js
@@ -0,0 +1,111 @@
+// import axios, { isCancel, AxiosError } from 'axios'
+
+
+export default () => ({
+
+
+	states: {
+		isLoading: false,
+		isError: false,
+	},
+
+
+	data: {
+		movie: {
+			title: '',
+			tagline: '',
+			genre: '',
+			director: '',
+			year: '',
+			image: {},
+		},
+		error: '',
+	},
+
+
+	/**
+	 * Initiate the good stuff
+	 *
+	 * @return {void}
+	 */
+	init() {
+		this.refreshMovie()
+	},
+
+
+	/**
+	 * Show (or refresh if already showing) movie
+	 *
+	 * @return {self}
+	 */
+	refreshMovie() {
+		this.showErrorMsg()
+		this.states.isLoading = true
+
+		this.fetchMovie().then( movie => {
+			this.data.movie = movie
+
+		}).catch( err => {
+			this.showErrorMsg( err.message )
+
+		}).finally( () => {
+			this.states.isLoading = false
+		})
+	},
+
+
+	/**
+	 * Fetch movies from backend
+	 *
+	 * @return {Promise}
+	 */
+	fetchMovie() {
+		return new Promise( ( resolve, reject ) => {
+			fetch( 'https://moovi.robr.app/endpoints/get-movie' )
+				.then( res => {
+					if ( !res.ok && 200 !== res.status ) {
+						throw new Error( 'Invalid response from server.' )
+					}
+
+					return res.json()
+
+				}).then( res => {
+					if ( 'object' !== typeof res && !Array.isArray( res.movies ) ) {
+						throw new Error( 'Unexpected response from server.' )
+					}
+
+					resolve( res.movies[0] )
+
+				}).catch( err => {
+					this.logErrMessage( err )
+					reject( new Error( 'Ran into an error. Please refresh the page!' ) )
+				})
+		})
+	},
+
+
+	/**
+	 * Show error message
+	 *
+	 * @param {string} msg Error message
+	 * @return {self}
+	 */
+	showErrorMsg( msg = '' ) {
+		this.data.error = msg
+		this.states.isError = !!msg
+
+		return this
+	},
+
+
+	/**
+	 * Log error to console
+	 *
+	 * @param {Error} err Error to log
+	 * @return {void}
+	 */
+	logErrMessage( err ) {
+		window.console.warn( `Moovi :: Error: "${err.message}"` )
+	},
+
+})
